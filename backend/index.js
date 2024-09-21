ce569889b60c91a15d609b3df87478b5762dd834
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-21 12:42:37 +0200

    Install Express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log('Hi from the backend!')
+const express = require('express')
+
+// Configurar App
+const app = express()
+
+// Definir ruta
+app.get('/', (req, res) => {
+    res.send('Hola')
+})
+
+// Definir puerto
+const PORT = process.env.PORT || 4000
+
+// Arrancar App
+app.listen(PORT, () => {
+    console.log('El servidor está escuchando en el puerto', PORT)
+})
