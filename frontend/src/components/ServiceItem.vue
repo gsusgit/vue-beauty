191ede13176af453126585a26652cb422526a131
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-22 10:16:43 +0200

    Select services for appointment

--- frontend/src/components/ServiceItem.vue
+++ frontend/src/components/ServiceItem.vue
@@ -15,10 +15,16 @@ const appointments = useAppointmentsStore()
 
 <template>
   <div
-      class="p-5 space-y-5 cursor-pointer bg-white rounded-lg"
+      :class="appointments.isServiceSelected(service._id) ? 'bg-green-500 text-white' : 'bg-white'"
+      class="p-5 space-y-5 cursor-pointer rounded-lg"
       @click="appointments.onServiceSelected(service)"
   >
     <p class="text-2xl font-light">{{service.name}}</p>
-    <p class="text-3xl font-black text-blue-700">{{formatCurrency(service.price)}}</p>
+    <p
+        :class="appointments.isServiceSelected(service._id) ? 'text-white' : 'text-gray-700'"
+        class="text-3xl font-black"
+    >
+      {{formatCurrency(service.price)}}
+    </p>
   </div>
 </template>
--- frontend/src/stores/appointments.js
+++ frontend/src/stores/appointments.js
@@ -1,12 +1,37 @@
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const useAppointmentsStore = defineStore('appointments', () =>{
+export const useAppointmentsStore = defineStore('appointments', () => {
+
+    const services = ref([])
 
     function onServiceSelected(service) {
-        console.log(service)
+        if (services.value.length === 2) {
+            alert('Máximo de servicios alcanzado para la cita')
+        } else {
+            if (services.value.some(s => s._id === service._id)) {
+                cancelSelectedService(service)
+            } else {
+                services.value.push(service)
+            }
+        }
     }
 
+    const cancelSelectedService = (service) => {
+        services.value.splice(service, 1)
+    }
+
+    const isServiceSelected = computed(() => {
+        return (id) => services.value.some(service => service._id === id)
+    })
+
+    const noServicesSelected = computed(() => {
+        return services.value.length === 0
+    })
+
     return {
-        onServiceSelected
+        onServiceSelected,
+        isServiceSelected,
+        noServicesSelected
     }
 })
