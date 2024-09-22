3e47740fcae055191cdd802f62280198000862f9
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-22 23:05:16 +0200

    Install Vue Tailwind Datepicker

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/vue-tailwind-datepicker/**/*.js',
   ],
   theme: {
     extend: {
@@ -11,6 +14,9 @@ export default {
       },
       backgroundImage: {
         'app': 'URL("/img/1.jpg")'
+      },
+      colors: {
+        'vtd-primary' : colors.blue
       }
     },
   },
