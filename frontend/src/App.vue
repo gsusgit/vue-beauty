3c821bb8aaec2b5fdce04dc4e6469cf7cd04d3e1
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-23 16:26:59 +0200

    Hide app background in small and medium devices

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import { RouterView } from 'vue-router'
 
 <template>
   <div class="md:flex h-screen">
-    <div class="h-64 md:h-screen bg-cover bg-center md:w-1/3 bg-app"></div>
+    <div class="h-64 hidden lg:block md:h-screen bg-cover bg-center md:w-1/3 bg-app"></div>
     <div class="md:w-2/3 px-10 py-5 min-h-full overflow-y-scroll">
       <RouterView />
     </div>
--- frontend/src/views/auth/AuthLayout.vue
+++ frontend/src/views/auth/AuthLayout.vue
@@ -6,7 +6,7 @@ const authRoutes = [
 </script>
 
 <template>
-  <div class="mx-auto lg:w-4/5 mt-20">
+  <div class="mx-auto lg:w-4/5 mt-10">
     <div class="flex items-center space-x-1 justify-center">
       <img src="@/assets/logo.svg" class="h-6" alt="Vue Logo">
       <h1 class="text-2xl lg:text-4xl whitespace-nowrap font-black text-white">ue Beauty</h1>
